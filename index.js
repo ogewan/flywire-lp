d0226238fe06dd5e1a09dbf49e4599839deda960
Seun Ogedengbe <ooo2@seungworkstation35.princeton.edu>
2019-08-08 15:57:49 -0400

    will did something

--- index.js
+++ index.js
@@ -1,85 +1,98 @@
 const comp = {
-  sticky: {
-    header: document.getElementById('myHeader'),
-    banner: document.getElementById('banner')
+  sticky: {},
+  fs_banner: {
+    index: 1,
+    auto: () => {
+      if (comp.targets.modal.style.display !== 'block') {
+        comp.lightbox.next();
+        return window.setTimeout(comp.fs_banner.auto, 7500);
+      }
+      return 0;
+    }
   },
-  fs_banner: {},
   lightbox: {
     index: 1,
     open: () => {
-      document.getElementById('myModal').style.display = 'block';
+      comp.targets.modal.style.display = 'block';
       document.body.style.overflow = 'hidden';
     },
     close: () => {
-      document.getElementById('myModal').style.display = 'none';
+      comp.targets.modal.style.display = 'none';
       document.body.style.overflow = 'auto';
+      window.setTimeout(comp.fs_banner.auto, 2000);
     },
-    next: () => this.show(this.index++),
-    prev: () => this.show(this.index--),
+    next: () => comp.lightbox.show(++comp.lightbox.index),
+    prev: () => comp.lightbox.show(--comp.lightbox.index),
     show: (n) => {
-      const
-        slides = document.getElementsByClassName('mySlides'),
-        max = slides.length;
+      const slides = comp.targets.slides, max = slides.length,
+            bslide = comp.targets.bslide;
 
-        if (!n) { n = max; }
-        else {n = n % max; }
-        this.index = n;
-
-        document.querySelector('.mySlides.active').classList.remove('active');
-        slides[n--].classList.add('active');
+      if (!n) {
+        n = max;
+      } else {
+        n = n % max || 1;
+      }
+      comp.lightbox.index = n;
+      /*deactivate old slide, activate new slide */
+      const active = comp.targets.active;
+      active.classList.remove('active');
+      comp.targets.active = slides[n-1];
+      comp.targets.active.classList.add('active');
+      /*deactivate old banner, activate new banner, reset old banner sizing,
+       * change new banner to reactive sizing*/
+      const bactive = comp.targets.bactive;
+      bactive.classList.remove('active');
+      comp.targets.bactive = bslide[n-1];
+      comp.targets.bactive.classList.add('active');
+      comp.targets.bactive.style.width = bactive.style.width;
+      comp.targets.bactive.style.height = bactive.style.height;
+      bactive.style.width = '';
+      bactive.style.height = '';
     }
   },
-};
-
-// When the user scrolls the page, execute myFunction
-window.onscroll =
-    () => {
-      const header = comp.sticky.header;
-      const bannerPos = comp.sticky.banner.getBoundingClientRect().bottom;
+  handlers: {
+    scroll: () => {
+      const header = comp.targets.header;
+      const bannerPos = comp.targets.banner.getBoundingClientRect().bottom;
       if (bannerPos < 0) {
         header.classList.add('sticky');
       } else {
         header.classList.remove('sticky');
       }
-    }
+    },
+    resize: () => {
+      const vw = window.innerWidth, vh = window.innerHeight,
+            bactive = comp.targets.bactive;
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function fwrd() {
-  showSlides(slideIndex++);
-}
-
-// Next/previous controls
-function back() {
-  showSlides(slideIndex--);
-}
-
-
-// Thumbnail image controls
-function slide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName('mySlides');
-  var dots = document.getElementsByClassName('demo');
-  var captionText = document.getElementById('caption');
-  if (n >= slides.length) {
-    slideIndex = 1
+      if (vw < vh) {
+        bactive.style.width = '100%';
+        bactive.style.height = 'auto';
+      } else {
+        bactive.style.width = 'auto';
+        bactive.style.height = '100%';
+      }
+    },
+  },
+  targets: {
+    set: false,
+    slides: Array.from(document.getElementsByClassName('mySlides')),
+    header: document.getElementById('myHeader'),
+    banner: document.getElementById('banner'),
+    bslide: Array.from(document.getElementsByClassName('b-slide')),
+    modal: document.getElementById('myModal'),
+    active: null,
+    bactive: null,
+  },
+  set: () => {
+    comp.targets.active =
+        comp.targets.slides.find(e => e.classList.contains('active'));
+    comp.targets.bactive =
+        comp.targets.bslide.find(e => e.classList.contains('active'));
   }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  // dots[slideIndex - 1].className += " active";
-  // captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+};
+
+window.onscroll = comp.handlers.scroll;
+window.onresize = comp.handlers.resize;
+comp.set();
+comp.handlers.resize();
+window.setTimeout(comp.fs_banner.auto, 7000);
