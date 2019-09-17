0ed1652a4f296874ea6fd49076d614c40ed2e4e2
Seun Ogedengbe <seun40@gmail.com>
2019-09-17 14:14:56 -0400

    finalized, need clean up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const comp = {
   sticky: {},
   fs_banner: {
-    index: 1,
+    index: 0,
     auto: () => {
       if (!(comp.targets.modal.style.display == 'block' ||
             comp.targets.signup.style.display == 'block')) {
@@ -12,7 +12,7 @@ const comp = {
     }
   },
   lightbox: {
-    index: 1,
+    index: 0,
     open: () => {
       comp.targets.modal.style.display = 'block';
       document.body.style.overflow = 'hidden';
@@ -25,25 +25,26 @@ const comp = {
     next: () => comp.lightbox.show(++comp.lightbox.index),
     prev: () => comp.lightbox.show(--comp.lightbox.index),
     show: (n) => {
-      const slides = comp.targets.slides, max = slides.length + 1,
+      const slides = comp.targets.slides, max = slides.length - 1,
             bslide = comp.targets.bslide;
 
-      if (!n) {
+      if (n < 0) {
         n = max;
-      } else {
-        n = n % max || 1;
+      } else if (n > max) {
+        n = 0;
       }
       comp.lightbox.index = n;
+      comp.targets.slidenum.innerText = `${n + 1} / 8`;
       /*deactivate old slide, activate new slide */
       const active = comp.targets.active;
       active.classList.remove('active');
-      comp.targets.active = slides[n - 1];
+      comp.targets.active = slides[n];
       comp.targets.active.classList.add('active');
       /*deactivate old banner, activate new banner, reset old banner sizing,
        * change new banner to reactive sizing*/
       const bactive = comp.targets.bactive;
       bactive.classList.remove('active');
-      comp.targets.bactive = bslide[n - 1];
+      comp.targets.bactive = bslide[n];
       comp.targets.bactive.classList.add('active');
       comp.targets.bactive.style.width = bactive.style.width;
       comp.targets.bactive.style.height = bactive.style.height;
@@ -90,13 +91,14 @@ const comp = {
   },
   targets: {
     set: false,
-    slides: Array.from(document.getElementsByClassName('mySlides')),
+    slides: Array.from(document.getElementsByClassName('slide')),
     header: document.getElementById('myHeader'),
     banner: document.getElementById('banner'),
     bslide: Array.from(document.getElementsByClassName('b-slide')),
     modal: document.getElementById('myModal'),
     signup: document.getElementById('signup'),
     signif: document.getElementById('signup_frame'),
+    slidenum: document.getElementById('slidenum'),
     active: null,
     bactive: null,
   },
@@ -108,9 +110,16 @@ const comp = {
   }
 };
 
-window.onscroll = comp.handlers.scroll;
+//window.onscroll = comp.handlers.scroll;
 window.onresize = comp.handlers.resize;
 comp.set();
 comp.handlers.resize();
+document.querySelector('dialog').addEventListener('click', e => {
+  e.stopPropagation();
+});
+
+/*document.getElementById('confirmSignup').addEventListener('click', e => {
+  e.stopPropagation();
+});*/
 comp.targets.signup.addEventListener('click', comp.handlers.clicksign);
 window.setTimeout(comp.fs_banner.auto, 7000);
